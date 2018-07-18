2ada84edfac99fe98bf8819a1e3edd52f2916352
Jhonatan Martins <jhonatanbmartins@gmail.com>
2018-07-18 20:54:34 -0300

    create directory structure for vue

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,12 +1,9 @@
-import Vue from 'vue'
-import App from '../app.vue'
-
+import Vue from 'vue/dist/vue.esm'
+import App from './app.vue'
+ 
 document.addEventListener('DOMContentLoaded', () => {
-  const el = document.body.appendChild(document.createElement('hello'))
-  const app = new Vue({
-    el,
-    render: h => h(App)
-  })
-
-  console.log(app)
+ const app = new Vue({
+  el: '#app',
+  render: h => h(App)
+ })
 })
