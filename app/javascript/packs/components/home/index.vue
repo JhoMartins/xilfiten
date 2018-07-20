eae90efe073557d874769f97986a9991b3495ed8
Jhonatan Martins <jhonatanbmartins@gmail.com>
2018-07-20 20:52:52 -0300

    create featured component

--- app/javascript/packs/components/home/index.vue
+++ app/javascript/packs/components/home/index.vue
@@ -1,16 +1,29 @@
-<template>
-  <div>
-   <h4 class="white--text">Wellcome to HomePage</h4>
-  </div>
-</template>
+   <template>
+     <div>
+        <Featured :movie="featuredMovie"/>
+     </div>
+   </template>
 
-<script>
- export default {
-  data () {
-   return { }
-  }
- }
-</script>
+  <script>
+    // ------- Dados Fake apenas para testarmos o layout -------- //
+    const featuredMovie = {
+                            id: 1,
+                            title: 'Criando uma API completa com Ruby On Rails',
+                            description: 'Saber como criar e consumir API’s é fundamental para qualquer programador, então nessa pequena série nós vamos ver o que é essencial para criar uma usando RoR.',
+                            featured_thumbnail_url: 'https://onebitcode.com/wp-content/uploads/2018/05/bg-example.png'
+                          }
+    import Featured from './_featured.vue'
+     export default {
+       data () {
+         return {
+            featuredMovie: featuredMovie,
+         }
+       },
+       components: {
+         'Featured': Featured
+       }
+     }
+   </script>
 
-<style scoped>
-</style>
+   <style scoped>
+   </style>
